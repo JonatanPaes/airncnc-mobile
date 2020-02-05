70ff833579d4d2b8eb72d9177bbc22182754b870
Jonatan Paes <jonatan.paes@itst.com.br>
2020-02-05 12:37:24 -0300

    subindo projeto

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
-
+import { YellowBox } from "react-native";
 import Routes from "./src/routes";
+
+YellowBox.ignoreWarnings(["Unrecognized WebSocket"]);
 
 //view = a div
 export default function App() {
